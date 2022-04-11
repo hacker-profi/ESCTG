6ef649cf071367a84b753b70c7e4b0bf7966b5aa
panda-coder-hmm <stastorlak@mail.ru>
2022-04-11 20:10:22 +0300

    ESCTG-1: add telegram bot

---
+++ src/main/java/ESCTB/Parametrs.java
@@ -0,0 +1,6 @@
+package ESCTB;
+
+public class Parametrs {
+    public static String username = "java_test_hmm_bot";
+    public static String token = "5214847045:AAHtZhijQc2suIal70VSX_d39cui2nzERCs";
+}
